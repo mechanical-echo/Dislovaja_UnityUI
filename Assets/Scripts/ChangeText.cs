b146ced905fbd5f19495ac4b36cd319568f4c981
sofia <sofija.dyshlovaya@gmail.com>
2023-04-24 15:57:09 +0300

    Izveidoju skriptu teksta uzglabasanai, pierakstiju TODO readme failaa

---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour {
+	public GameObject InputName;
+	public GameObject InputAge;
+	public GameObject OutputNameAge;
+	private string Name;
+	private int Age;
+
+	public void ApplyText()
+	{
+		try
+		{
+			Name = InputName.GetComponent<Text>().text;
+			Age = int.Parse(InputAge.GetComponent<Text>().text);
+			OutputNameAge.GetComponent<Text>().text = Name + " ir "+Age+" gadu vecs !";
+		}
+		catch (Exception e) {
+			Debug.LogError(e.Message);
+            throw;
+        };
+	}
+	
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PD-unity-character-customization
+TODO
+- [x] Toggle group
+- [ ] Input fields
+- [ ] Class choice
+- [ ] Start menu
+- [ ] Change background along with characters
