9dd9d2a7d2a5a70bc9b2527c0c43f0243adb1c1b
Sofija <sofija.dyshlovaya@gmail.com>
2023-05-01 17:41:09 +0300

    Izveidoju ainu parslegsanu

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public void GoToStart()
+    {
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+
+    public void GoToCharacters()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void Stop()
+    {
+        Application.Quit();
+    }
+}
